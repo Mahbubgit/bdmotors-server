086d73077e7552001d5d7f68f5c8086fc3c47ec1
Your Name <you@example.com>
2022-06-14 18:14:49 +0600

    featured product and pagination added

--- index.js
+++ index.js
@@ -13,37 +13,55 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w5fhw.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-	try{
+async function run() {
+    try {
         await client.connect();
         const productCollection = client.db('bdMotors').collection('product');
+        const featureProductCollection = client.db('bdMotors').collection('featureProduct');
 
-        app.get('/product', async(req, res) =>{
+        app.get('/product', async (req, res) => {
+            let products;
+
+            console.log('query', req.query);
+            const selectedPage = parseInt(req.query.selectedPage);
+            const pageLoadSize = parseInt(req.query.pageLoadSize);
+
             const query = {};
             const cursor = productCollection.find(query);
-            const products = await cursor.toArray();
+
+            if (selectedPage || pageLoadSize) {
+                products = await cursor.skip(selectedPage * pageLoadSize).limit(pageLoadSize).toArray();
+            }
+            else {
+                products = await cursor.toArray();
+            }
             // const products = await cursor.limit(5).toArray();
             res.send(products);
         });
 
-        app.get('/productCount', async(req, res) =>{
+        app.get('/featureProduct', async (req, res) => {
             const query = {};
-            const cursor = productCollection.find(query);
-            const count = await cursor.count();
-            res.send({count});
-        })
-	}
-	finally{
+            const cursor = featureProductCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        });
 
-	}
+        app.get('/productCount', async (req, res) => {
+            const count = await productCollection.estimatedDocumentCount();
+            res.send({ count });
+        })
+    }
+    finally {
+
+    }
 }
 run().catch(console.dir);
 
 
-app.get('/', (req, res) =>{
-res.send('BDMOTORS is running and waiting')
+app.get('/', (req, res) => {
+    res.send('BDMOTORS is running and waiting')
 });
 
-app.listen(port, () =>{
-console.log('BDMOTORS is running on port', port);
+app.listen(port, () => {
+    console.log('BDMOTORS is running on port', port);
 })
