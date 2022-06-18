87fc7dca1a0bcffc6570383a6aab994555be55ce
Your Name <you@example.com>
2022-06-18 20:41:58 +0600

    show my added item

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
+const res = require('express/lib/response');
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
+// Verify JSON Web Token
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+        next();
+    })
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w5fhw.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -18,6 +36,15 @@ async function run() {
         await client.connect();
         const productCollection = client.db('bdMotors').collection('product');
         const featureProductCollection = client.db('bdMotors').collection('featureProduct');
+
+        // Authentication
+        // app.post('/login', async (req, res) => {
+        //     const user = req.body;
+        //     const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        //         expiresIn: '1d'
+        //     });
+        //     res.send({ accessToken });
+        // })
 
         // for pagination
         app.get('/product', async (req, res) => {
@@ -35,7 +62,6 @@ async function run() {
             else {
                 products = await cursor.toArray();
             }
-            // const products = await cursor.limit(6).toArray();
             res.send(products);
         });
 
@@ -118,16 +144,40 @@ async function run() {
             if (result.deletedCount === 1) {
                 console.log("Successfully deleted one document.");
             }
-
             res.send(result);
         });
 
+        // verify email by JWT Token
+
+        // app.get('/product', verifyJWT, async (req, res) => {
+        //     const decodedEmail = req.decoded.email;
+        //     const email = req.query.email;
+        //     if (email === decodedEmail) {
+        //         const query = { email: email };
+        //         const cursor = productCollection.find(query);
+        //         const result = await cursor.toArray();
+        //         res.send(result);
+        //     }
+        //     else {
+        //         res.status(403).send({ message: 'forbidden access' })
+        //     }
+        // })
+
         // For add a new item
-        app.post('/product/addItem', async (req, res) => {
+        app.post('/product', async (req, res) => {
             const addNewItem = req.body;
             const result = await productCollection.insertOne(addNewItem);
             res.send(result);
         });
+
+        app.get('/product/myItem', async (req, res) => {
+            const email = req.query.email;
+            const query = {email: email};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+
     }
     finally {
 
